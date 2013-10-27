8e8f6deb0ed100ca2f53e45f7d37c4bb91eff10b
Rafael Lopes <dev@rafalopes.com.br>
2013-10-27 00:23:28 -0200

    Adding the script that draws the graph itselfs.

---
+++ aws_cloudfront/graph.php
@@ -0,0 +1,27 @@
+<?php
+include("c.php");
+?>
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+        <?=GoogleGraphStr();?>
+        ]);
+
+        var options = {
+          title: 'AWS CloudFront Usage according to what will be billed'
+        };
+
+        var chart = new google.visualization.<?=$graph;?>Chart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="chart_div" style="width: 10900px; height: 900px;"></div>
+  </body>
+</html>
