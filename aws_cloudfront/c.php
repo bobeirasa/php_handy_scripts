7af833c3fd65884159fd7d9658441a7968fc3791
Rafael Lopes <dev@rafalopes.com.br>
2013-10-27 00:23:11 -0200

    Adding the script that is invoked to get XML elements and prepare for Google Chart

---
+++ aws_cloudfront/c.php
@@ -0,0 +1,32 @@
+<?php
+$xml=simplexml_load_file("report.xml");
+
+$InitialTime = strtotime((string)$xml->OperationUsage[0]->StartTime);
+$Grain = strtotime((string)$xml->OperationUsage[0]->EndTime) - strtotime((string)$xml->OperationUsage[0]->StartTime);
+$graph = $Grain > 3600 ? "Column" : "Line";
+
+foreach($xml->OperationUsage as $Item)
+{
+    if( preg_match('/DataTransfer/',$Item->UsageType) )
+    {
+        $usage[(strtotime($Item->StartTime)-$InitialTime)/$Grain] += floatval( (string)$Item->UsageValue );
+    }
+}
+function GoogleGraphStr()
+{
+    function assembleDate($offset)
+    {
+        global $InitialTime, $graph, $Grain;
+        $format="d/m";
+        if($graph == "Line") $format.=" H:i";
+        return date($format,$InitialTime+($offset*$Grain));
+    }
+    global $usage;
+    $result="['Date', 'Terabytes']";
+    foreach($usage as $i=>$B)
+    {
+        $result.=", ['". assembleDate($i) ."', ". round(pow($B,4),2) ."]";
+    }
+    return $result;
+}
+?>
